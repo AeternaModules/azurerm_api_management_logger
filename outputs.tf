6c36f5ad2ebc97cb65c5a645f181a24df8554e4e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,40 +1,35 @@
-output "api_management_loggers" {
-  description = "All api_management_logger resources"
-  value       = azurerm_api_management_logger.api_management_loggers
-  sensitive   = true
-}
 output "api_management_loggers_api_management_name" {
-  description = "List of api_management_name values across all api_management_loggers"
-  value       = [for k, v in azurerm_api_management_logger.api_management_loggers : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_loggers, keyed the same as var.api_management_loggers"
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.api_management_name }
 }
 output "api_management_loggers_application_insights" {
-  description = "List of application_insights values across all api_management_loggers"
-  value       = [for k, v in azurerm_api_management_logger.api_management_loggers : v.application_insights]
+  description = "Map of application_insights values across all api_management_loggers, keyed the same as var.api_management_loggers"
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.application_insights }
   sensitive   = true
 }
 output "api_management_loggers_buffered" {
-  description = "List of buffered values across all api_management_loggers"
-  value       = [for k, v in azurerm_api_management_logger.api_management_loggers : v.buffered]
+  description = "Map of buffered values across all api_management_loggers, keyed the same as var.api_management_loggers"
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.buffered }
 }
 output "api_management_loggers_description" {
-  description = "List of description values across all api_management_loggers"
-  value       = [for k, v in azurerm_api_management_logger.api_management_loggers : v.description]
+  description = "Map of description values across all api_management_loggers, keyed the same as var.api_management_loggers"
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.description }
 }
 output "api_management_loggers_eventhub" {
-  description = "List of eventhub values across all api_management_loggers"
-  value       = [for k, v in azurerm_api_management_logger.api_management_loggers : v.eventhub]
+  description = "Map of eventhub values across all api_management_loggers, keyed the same as var.api_management_loggers"
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.eventhub }
   sensitive   = true
 }
 output "api_management_loggers_name" {
-  description = "List of name values across all api_management_loggers"
-  value       = [for k, v in azurerm_api_management_logger.api_management_loggers : v.name]
+  description = "Map of name values across all api_management_loggers, keyed the same as var.api_management_loggers"
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.name }
 }
 output "api_management_loggers_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_loggers"
-  value       = [for k, v in azurerm_api_management_logger.api_management_loggers : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_loggers, keyed the same as var.api_management_loggers"
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.resource_group_name }
 }
 output "api_management_loggers_resource_id" {
-  description = "List of resource_id values across all api_management_loggers"
-  value       = [for k, v in azurerm_api_management_logger.api_management_loggers : v.resource_id]
+  description = "Map of resource_id values across all api_management_loggers, keyed the same as var.api_management_loggers"
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.resource_id }
 }
 
