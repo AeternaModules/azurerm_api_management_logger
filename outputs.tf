c73ce92551ae22b6c9a6b6d17b7f798114c5ebab
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:46:06 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,39 @@
 output "api_management_loggers_id" {
   description = "Map of id values across all api_management_loggers, keyed the same as var.api_management_loggers"
-  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.id }
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "api_management_loggers_api_management_name" {
   description = "Map of api_management_name values across all api_management_loggers, keyed the same as var.api_management_loggers"
-  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.api_management_name }
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.api_management_name if v.api_management_name != null && length(v.api_management_name) > 0 }
 }
 output "api_management_loggers_application_insights" {
   description = "Map of application_insights values across all api_management_loggers, keyed the same as var.api_management_loggers"
-  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.application_insights }
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.application_insights if v.application_insights != null && length(v.application_insights) > 0 }
   sensitive   = true
 }
 output "api_management_loggers_buffered" {
   description = "Map of buffered values across all api_management_loggers, keyed the same as var.api_management_loggers"
-  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.buffered }
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.buffered if v.buffered != null }
 }
 output "api_management_loggers_description" {
   description = "Map of description values across all api_management_loggers, keyed the same as var.api_management_loggers"
-  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.description }
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.description if v.description != null && length(v.description) > 0 }
 }
 output "api_management_loggers_eventhub" {
   description = "Map of eventhub values across all api_management_loggers, keyed the same as var.api_management_loggers"
-  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.eventhub }
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.eventhub if v.eventhub != null && length(v.eventhub) > 0 }
   sensitive   = true
 }
 output "api_management_loggers_name" {
   description = "Map of name values across all api_management_loggers, keyed the same as var.api_management_loggers"
-  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.name }
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "api_management_loggers_resource_group_name" {
   description = "Map of resource_group_name values across all api_management_loggers, keyed the same as var.api_management_loggers"
-  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.resource_group_name }
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "api_management_loggers_resource_id" {
   description = "Map of resource_id values across all api_management_loggers, keyed the same as var.api_management_loggers"
-  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.resource_id }
+  value       = { for k, v in azurerm_api_management_logger.api_management_loggers : k => v.resource_id if v.resource_id != null && length(v.resource_id) > 0 }
 }
 
